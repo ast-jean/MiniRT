54a5a406c2aba827fc8871744ab67eda1cc871ff
ast-jean <ast-jean@student.42quebec.com>
2023-03-02 13:09:45 -0500

    update .h with 3d 2d env + obj structs

--- include/miniRT.h
+++ include/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: ast-jean <ast-jean@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:58:16 by ast-jean          #+#    #+#             */
-/*   Updated: 2023/03/01 11:49:00 by ast-jean         ###   ########.fr       */
+/*   Updated: 2023/03/02 13:08:58 by ast-jean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,97 @@
 # include <unistd.h>
 # include <signal.h>
 # include <stdlib.h>
+# include <stdint.h>
 # include <limits.h>
 # include  <fcntl.h>
 # include  <math.h>
 # include "MLX42/include/MLX42/MLX42.h"
-
 # include "libft/libft.h"
-
-
 
 #define BLACK	0x00000000
 #define WHITE	0xFFFFFFFF
-#define GRAY	0x00000088
+#define GRAY	0x888888FF
 #define RED		0xFF0000FF
 #define GREEN	0x00FF00FF
 #define BLUE	0x0000FFFF
 
-typedef struct s_vars
+typedef struct s_Fixed{
+	//slavoie
+}	t_Fixed;
+
+typedef struct s_2dPoint{
+    t_Fixed x;
+	t_Fixed y;
+} t_2dPoint;
+
+typedef struct s_3dPoint{
+    t_Fixed x;
+	t_Fixed y;
+	t_Fixed z;
+	int32_t		color;
+} t_3dPoint;
+
+typedef struct s_Vars
 {
-	void	*mlx;
-	void	*mlx_win;
-}			t_vars;
+	void	*objs;
+	// all of our values needed throught the program
+}			t_Vars;
 
-typedef struct s_point{
-    float x;
-	float y;
-} t_Point;
+typedef struct s_AmbientLight
+{
+	char	*id; // always 'A'
+	t_Fixed	light_ratio; //from 0.0 to 1.0
+	uint8_t	color; //RGB from 0 to 255
+}	t_AmbientLight;
 
+typedef struct s_Camera
+{
+	char		*id;		 // always 'C'
+	t_3dPoint	orientation; //from -1 to 1 for each xyz
+	uint8_t		FOV;		 //FOV from 0 to 180
+}	t_Camera;
+
+typedef struct s_Light
+{
+	char	*id; // always 'L'
+	t_Fixed	light_ratio; //from 0.0 to 1.0
+	int	color; //RGB from 0 to 255
+}	t_Camera;
+
+typedef struct s_Sphere
+{
+	char		*id; // always 'sp'
+	t_3dPoint	center; //coords for center xyz
+	t_Fixed		diameter; //size of sphere
+	uint8_t		color; //RGB from 0 to 255
+}	t_Sphere;
+
+typedef struct s_Plane
+{
+	char		*id; // always 'pl'
+	t_3dPoint	coords; //coords for center xyz
+	t_3dPoint	orientation; //from -1 to 1 for each xyz
+	uint8_t		color; //RGB from 0 to 255
+}	t_Plane;
+
+typedef struct s_Cylinder
+{
+	char		*id; // always 'cy'
+	t_3dPoint	coords; //coords for center xyz
+	t_3dPoint	orientation; //from -1 to 1 for each xyz
+	t_Fixed		dia; //diameter of cercles
+	t_Fixed		height; // height of cylinder
+	uint8_t		color; //RGB from 0 to 255
+}	t_Cylinder;
+
+typedef struct s_Cone
+{
+	char		*id; // always 'co'
+	t_3dPoint	coords; //coords for center xyz
+	t_3dPoint	orientation; //from -1 to 1 for each xyz
+	t_Fixed		dia; //diameter of cercle
+	t_Fixed		height; // height of cylinder
+	uint8_t		color; //RGB from 0 to 255
+}	t_Cone;
 
 #endif
