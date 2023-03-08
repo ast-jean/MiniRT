dd2810df0f6de23c7834e9bf4bcafd2322db41ee
Maya Tremblay <mtrembla@student.42quebec.com>
2023-03-08 16:08:30 -0500

    Ready to merge to main

--- srcs/parsing/objects.c
+++ srcs/parsing/objects.c
@@ -3,7 +3,7 @@
 t_shape   *object_A(char **elem)
 {
     t_shape *A = malloc(sizeof(t_shape));
-    // A->id = elem[0];
+    A->id = elem[0];
     // A->light_ratio = elem[1];
     // A->color = elem[2];
     return(A);
