1848557948322647110b4dd48bee5a01b0ac1b6e
Adam St-jean lejeune <ast-jean@c3r7p4.42quebec.com>
2023-05-17 13:58:05 -0400

    Ambient light final fix(i hope)

--- srcs/rayTracing/colors.c
+++ srcs/rayTracing/colors.c
@@ -6,7 +6,7 @@
 /*   By: ast-jean <ast-jean@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:56:37 by ast-jean          #+#    #+#             */
-/*   Updated: 2023/05/15 16:18:58 by ast-jean         ###   ########.fr       */
+/*   Updated: 2023/05/17 13:55:23 by ast-jean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,16 @@ t_rgba	mix_colors_light(t_Ray_hit hit, t_shape shape, double coeff)
 		coeff = a_r;
 	else
 	{
-		result = mix_colors(result, remove_excess(\
-		separate_color_rgba(init_vars()->light->color)), l_r);
-		coeff += a_r;
+		// printf("Bef=r.%d, g.%d, b.%d \n", result.r, result.g, result.b);
+		
+		result = mix_colors(result, remove_excess(separate_color_rgba(init_vars()->light->color)), l_r);
+		coeff = coeff * l_r + a_r;
+	
+		// printf("After=r.%d, g.%d, b.%d \n", result.r, result.g, result.b);
 	}
-	result = mix_colors(result, remove_excess(\
-	separate_color_rgba(init_vars()->ambient_light->color)), a_r);
+	result = mix_colors(result, (separate_color_rgba(init_vars()->ambient_light->color)), a_r);
 	result = brightness(result, coeff);
+
 	if (specular_coeff > 0 && !hit.hit)
 		result = rgba_add(result, \
 		specular(result, pow(specular_coeff, 100), l_r));
@@ -67,10 +70,17 @@ t_rgba	mix_colors(t_rgba color1, t_rgba color2, double mix_factor)
 {
 	t_rgba	result;
 
+	static int g = 0;
+
+color2.r=(255 - color2.r);
+color2.g=(255 - color2.g);
+color2.b=(255 - color2.b);
+
+color2 = remove_excess(color2);
 	mix_factor = fmax(0.0, fmin(1.0f, mix_factor));
-	result.r = (uint8_t)(color1.r + color2.r * mix_factor);
-	result.g = (uint8_t)(color1.g + color2.g * mix_factor);
-	result.b = (uint8_t)(color1.b + color2.b * mix_factor);
+	result.r = (uint8_t)((color1.r) + ((color2.r)  * mix_factor));
+	result.g = (uint8_t)((color1.g) + ((color2.g)  * mix_factor));
+	result.b = (uint8_t)((color1.b) + ((color2.b)  * mix_factor));
 	result.a = 255;
 	return (result);
 }
