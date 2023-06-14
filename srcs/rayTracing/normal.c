37ab09897597829c9d0731fbca3c306ea89e7dc7
Steven-emmanuel Lavoie <slavoie@c1r2p2.42quebec.com>
2023-06-14 15:39:00 -0400

    fix light inside cylindre

--- srcs/rayTracing/normal.c
+++ srcs/rayTracing/normal.c
@@ -6,7 +6,7 @@
 /*   By: slavoie <slavoie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 19:55:10 by slavoie           #+#    #+#             */
-/*   Updated: 2023/06/14 09:24:42 by slavoie          ###   ########.fr       */
+/*   Updated: 2023/06/14 15:33:53 by slavoie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ t_vector3d	light_normal(t_vector3d coords, t_vector3d obj_coord)
 }
 
 t_vector3d	cylinder_normal(t_vector3d intersection, \
-t_vector3d C, t_vector3d V)
+t_vector3d C, t_vector3d V, double radius)
 {
 	t_vector3d	intersection_to_center;
 	t_vector3d	proj_axis_vec;
 	t_vector3d	perpendicular_vec;
 	double		scalar_proj;
 	t_vector3d	light_dir;
+	t_vector3d	light_to_center;
+	double		distance_to_axis;
 
 	light_dir = vector3d_sub(point3d_to_vector3d \
 	(init_vars()->light->coord), intersection);
@@ -32,10 +34,44 @@ t_vector3d C, t_vector3d V)
 	scalar_proj = vector3d_dot(intersection_to_center, V);
 	proj_axis_vec = vector3d_mult(V, scalar_proj);
 	perpendicular_vec = vector3d_sub(intersection_to_center, proj_axis_vec);
-	if (vector3d_dot(perpendicular_vec, light_dir) < 0)
+
+	// Calculate the distance from the light source to the cylinder axis
+	light_to_center = vector3d_sub(point3d_to_vector3d(init_vars()->light->coord), C);
+	distance_to_axis = vector3d_length(vector3d_sub(light_to_center, vector3d_mult(V, vector3d_dot(light_to_center, V))));
+
+	// If the light is inside the cylinder, invert the normal
+	if (distance_to_axis < radius)
 		perpendicular_vec = vector3d_mult(perpendicular_vec, -1);
+
 	return (vector3d_norm(perpendicular_vec));
 }
+
+
+// t_vector3d	cylinder_normal(t_vector3d intersection, \
+// t_vector3d C, t_vector3d V)
+// {
+// 	t_vector3d	intersection_to_center;
+// 	t_vector3d	proj_axis_vec;
+// 	t_vector3d	perpendicular_vec;
+// 	double		scalar_proj;
+// 	t_vector3d	light_dir;
+
+
+// 	light_dir = vector3d_sub(intersection, point3d_to_vector3d \
+// 	(init_vars()->light->coord));
+// 	// light_dir = vector3d_sub(point3d_to_vector3d \
+// 	// (init_vars()->light->coord), intersection);
+// 	intersection_to_center = vector3d_sub(intersection, C);
+// 	scalar_proj = vector3d_dot(intersection_to_center, V);
+// 	proj_axis_vec = vector3d_mult(V, scalar_proj);
+// 	perpendicular_vec = vector3d_sub(intersection_to_center, proj_axis_vec);
+// 	// if (vector3d_dot(perpendicular_vec, light_dir) < 0)
+// 	// 	perpendicular_vec = vector3d_mult(perpendicular_vec, -1);
+// 	// return (vector3d_norm(perpendicular_vec));
+// 		// if (vector3d_dot(intersection, perpendicular_vec ) < 0)
+// 		// 	return(vector3d_norm(vector3d_mult(perpendicular_vec, -1)));
+// 		return (vector3d_norm(perpendicular_vec));
+// }
 
 t_vector3d	plane_normal(t_vector3d hit_coords, t_vector3d orientation)
 {
@@ -77,7 +113,7 @@ t_vector3d obj_coord, t_shape shape, t_vector3d light_ray)
 		return (plane_normal(coords, point3d_to_vector3d(shape.orientation)));
 	else if (ft_strcmp(shape.id, "cy"))
 		return (cylinder_normal(coords, obj_coord,
-				point3d_to_vector3d(shape.orientation)));
+				vector3d_norm(point3d_to_vector3d(shape.orientation)), to_double(shape.radius)));
 	else
 		return (vector3d_init(0, 0, 0));
 }
